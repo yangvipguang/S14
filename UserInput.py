a0a4922422ccdd99a42a3d03a7284398f0dd4e7d
yangvipguang <yangvipguang@126.com>
2017-12-20 01:33:13 +0800

    用户输入小程序三种不同方式

--- UserInput.py
+++ UserInput.py
@@ -11,5 +11,22 @@ Age:%d
 Job:%s
 Salary:%s
 """  % (name, name, age, job, salary)
-print(info)
+#print(info)
 
+info2 = """
+-------------info2 of {name}------
+Name:{name}
+Age:{age}
+Job:{job}
+Salary:{salary}
+"""
+#print(info)
+
+info3 = """
+-------------info3 of {_name}------
+Name:{_name}
+Age:{_age}
+Job:{_job}
+Salary:{_salary}
+""".format(_name=name,_job=job,_age=age,_salary=salary)
+print(info)
