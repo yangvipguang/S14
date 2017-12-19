cf5cf1256013255f5a2726e16adb51a8a88ddb96
yangvipguang <yangvipguang@126.com>
2017-12-20 01:18:38 +0800

    用户输入小程序

---
+++ UserInput.py
@@ -0,0 +1,15 @@
+name = input("name:")
+age = int(input("age:"))
+job = input("job:")
+salary = input("salary:")
+
+
+info = """
+-------------info of %s------
+Name:%s
+Age:%d
+Job:%s
+Salary:%s
+"""  % (name, name, age, job, salary)
+print(info)
+
